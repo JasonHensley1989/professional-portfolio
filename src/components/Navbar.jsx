64b427f6f7a5cfd08ddd81b302f59de7b218d640
Jason <jasonhensley1989@gmail.com>
2022-04-20 00:22:20 -0400

    hamburger added basic layout created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/logo.png';
+
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a129f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="logo" style={{width: '50px'}} />
+        </div>
+
+        {/* menu */}
+
+        <div>
+            <ul className='flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>My Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* hamburger */}
+        <div className='hidden'>
+            <FaBars />
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>My Work</li>
+            <li>Contact</li>
+        </ul>
+
+         {/* Social Media Icons */}
+         <div className='hidden'>
+
+         </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
