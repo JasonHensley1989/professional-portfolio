a45cd52e40fd82881eed010d0582c76b05789625
Jason <jasonhensley1989@gmail.com>
2022-04-20 16:19:13 -0400

    message system implemented for contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div>
       <div name='contact' className='w-full h-screen bg-[#f2efea] flex justify-center items-center p-4'>
-          <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
+          <form method='POST' action="https://getform.io/f/5d326166-8d8c-43d5-89ad-6d7da330da01" className='flex flex-col max-w-[600px] w-full'>
               <div className='pb-8'>
                   <p className='text-4xl font-bold inline border-b-2 border-[#557373] text-[#557373]'>Contact</p>
                   <p className='text-[#0d0d0d] py-4'>Submit the form below or send me an email - jasonhensley1989@gmail.com</p>
