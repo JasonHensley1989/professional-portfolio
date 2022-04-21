67863b06a51a6ce7f91d957c2f205605aae830af
Jason <jasonhensley1989@gmail.com>
2022-04-20 23:59:06 -0400

    work margin edit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -24,7 +24,7 @@ const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#f2efea] text-[#557373] pt-10'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-2 border-[#557373]'>Skills</p>
               <p className='py-4 text-[#0d0d0d]'> These are the technologies I've worked with</p>
