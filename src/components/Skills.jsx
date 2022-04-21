79ce8cad8cc521e8faab53fbcf020776f3cf0f55
Jason <jasonhensley1989@gmail.com>
2022-04-20 23:50:39 -0400

    Skills margin edit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-// import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-// import FireBase from '../assets/firebase.png';
-import AWS from '../assets/aws.png';
-import GitHub from '../assets/github.png';
-// import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
 import { 
     FaJsSquare,
     FaHtml5,
@@ -31,9 +21,9 @@ const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#f2efea] text-[#557373] pt-10'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-[80%]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full'>
           <div>
-              <p className='text-4xl font-bold inline border-b-2 border-[#557373] '>Skills</p>
+              <p className='text-4xl font-bold inline border-b-2 border-[#557373]'>Skills</p>
               <p className='py-4 text-[#0d0d0d]'> These are the technologies I've worked with</p>
           </div>
 
