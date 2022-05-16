737d91da585e4512a2a4a1df726f88ff968e2d0a
Jason <jasonhensley1989@gmail.com>
2022-05-16 01:51:23 -0400

    resume view added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,11 @@ import {
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import '../assets/JHResume.pdf';
+
 const Footer = () => {
+
+
     return (
         <div className='w-full h-screen/2 bg-[#557373] p-4 text-[#f2efea] flex-row'>
             <div className='flex flex-row justify-center space-x-10'>
@@ -14,7 +18,7 @@ const Footer = () => {
                 <a href='https://www.linkedin.com/in/jason-hensley1989/'><FaLinkedin size={30} className='hover:scale-110 duration-500'/></a>
                 <a href='https://codepen.io/JasonHensley1989/'><FaCodepen size={30} className='hover:scale-110 duration-500'/></a>
                 <a href='/'><HiOutlineMail size={30} className='hover:scale-110 duration-500'/></a>
-                <a href='https://docs.google.com/document/d/1CXi9UQuzz4pr7v0ADOfGseEEJywdKIJaiGKd54OPm7c/edit?usp=sharing'><BsFillPersonLinesFill size={30} className='hover:scale-110 duration-500'/><span className='mx-[-10px]'>Resume</span></a>              
+                <a href='https://drive.google.com/file/d/11lYl-DvQnMrqCpqcvXGTrAAbceTeuCso/view?usp=sharing' download='https://drive.google.com/file/d/11lYl-DvQnMrqCpqcvXGTrAAbceTeuCso/view?usp=sharing'><BsFillPersonLinesFill size={30} className='hover:scale-110 duration-500'/><span className='mx-[-10px]'>Resume</span></a>              
             </div>     
         </div>
     )
